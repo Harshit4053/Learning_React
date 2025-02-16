246be722501eea30ce1f5dc9377f4443b0a1a90b
Harshit4053 <harshitgajipara204@gmail.com>
2025-02-16 15:04:26 +1000

    Counter Project with Hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,21 +4,46 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let counter = 15
+
+  const [counter, setCounter] = useState(0)
+  const [error, seterror] = useState("")
+
+  
 
   const addValue = () => {
-    counter = counter + 1
-    console.log("Added", counter)
+    if(counter <20){
+      setCounter(counter + 1)
+      
+    }else{
+      seterror("Maximum limit reached");
+      
+    }
+    
+    
+  }
+
+  const removeValue = () => {
+    if(counter >= 1){
+      setCounter(counter - 1)
+      
+    }else{
+      seterror("Minimum limit reached");
+     
+    }
+    
+   
   }
 
   return (
     <>
       <h1>Welcome to React</h1>
       <h2>Counter value : {counter}</h2>
-      
+
       <button onClick={addValue}>Add Value</button>
       <br />
-      <button>Remove Value</button>
+      <button onClick={removeValue}>Remove Value</button>
+      <p>Result : {counter}</p>
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </>
   )
 }
